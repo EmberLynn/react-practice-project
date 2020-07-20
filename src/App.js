c076082224aa095ff60ea3ad1a7ae01cdccc0ab5
EmberLynn <ember.robb@gmail.com>
2020-07-20 09:50:03 -0600

    Added a component and passed it some props.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!  This is a React App!</h1>
+        <h1>Hello World!</h1>
+        <p>This is a React App</p>
+        <Person name="John" age="50"/>
+        <Person name="Sherlock" age="41">My Hobbies: Solving Murders</Person>
+        <Person name="Mrs.H" age="114"/>
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'))
   }
 }
 
