894a4c3e59fc8fcb5f2154a2f8992ca27d1cd706
EmberLynn <ember.robb@gmail.com>
2020-07-20 12:46:30 -0600

    Handling events and chaning states.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'John', age: 50},
+      { name: 'Sherlock', age: 41},
+      { name: 'Mrs.H', age: 114 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: 'Watson', age: 50},
+        { name: 'Holmes', age: 41},
+        { name: 'Mrs.H', age: 115 }
+      ]
+     })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
         <p>This is a React App</p>
-        <Person name="John" age="50"/>
-        <Person name="Sherlock" age="41">My Hobbies: Solving Murders</Person>
-        <Person name="Mrs.H" age="114"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Solving Murders</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'))
