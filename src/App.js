5c692614298eff07f653dcc0eb22814ea62dc4b7
EmberLynn <ember.robb@gmail.com>
2020-07-25 08:53:35 -0600

    Delete a person from list when clicked

--- src/App.js
+++ src/App.js
@@ -12,15 +12,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    //console.log('Was clicked!');
-    this.setState({
-      persons: [
-        { name: newName, age: 50},
-        { name: 'Holmes', age: 41},
-        { name: 'Mrs.H', age: 115 }
-      ]
-     })
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   nameChangedHandler = (event) => {
@@ -53,8 +48,9 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return <Person 
+            click={() => this.deletePersonHandler(index)}
             name={person.name} 
             age={person.age}/>
           })}
