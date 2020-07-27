6844bafd9585fcd8336b73cd56058aae8905abf3
EmberLynn <ember.robb@gmail.com>
2020-07-27 13:04:06 -0600

    Flexible list that updates name dynamically

--- src/App.js
+++ src/App.js
@@ -19,14 +19,20 @@ class App extends Component {
     this.setState({persons: persons})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'John', age: 50},
-        { name: event.target.value, age: 41},
-        { name: 'Mrs.H', age: 115 }
-      ]
-     })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+        return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   togglePersonsHandler = () =>{
@@ -54,7 +60,8 @@ class App extends Component {
             click={() => this.deletePersonHandler(index)}
             name={person.name} 
             age={person.age}
-            key={person.id}/>
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
