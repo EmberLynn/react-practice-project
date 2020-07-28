e888e65bb01ed0076bb1c9c3d25784711e7a3e4e
EmberLynn <ember.robb@gmail.com>
2020-07-27 18:38:35 -0600

    Added comments to explain nameChangedHandler

--- src/App.js
+++ src/App.js
@@ -20,18 +20,40 @@ class App extends Component {
   }
 
   nameChangedHandler = (event, id) => {
+    //find index returns the index of an element in an array
+    //Like map, it takes a function which will be executed on each item in the array
+    //p is the current element in the array
+    //we return true or false depending if the element was or was not found
     const personIndex = this.state.persons.findIndex(p => {
+        //if the current element's id is the same as the id passed into the method, return true
+        //once returned true, personIndex will hold the position of the found element
         return p.id === id;
     });
 
+    //do not do this:
+    //const person = this.state.persons[personIndex]
+    //when you are dealing with the state, avoid directly mutating it
+    //instead, make a copy and merge the state
+
+    //use the spread operator to create a new object from the old object
     const person = {
       ...this.state.persons[personIndex]
     };
 
+    //update the person's name which is the data we are changing
+    //we grab this from the onChange event
     person.name = event.target.value;
+
+    //update the array
+    //make new array from old by using the spread opertor
     const persons = [...this.state.persons];
+
+    //update the person in the array at the correct index
+    //we are replacing person at personIndex with our newly changed person
     persons[personIndex] = person;
 
+    //now that everything has been changed, let's set the state
+    //persons in state will now equal this new persons array
     this.setState({persons: persons})
   }
 
