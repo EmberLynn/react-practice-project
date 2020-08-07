d9ff92a146e363f931d1aee05adc269d4bad3604
EmberLynn <ember.robb@gmail.com>
2020-08-07 10:07:42 -0600

    Added dynamic styles and radium for hover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -66,11 +67,16 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -87,12 +93,25 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'pink',
+        color: 'black'
+      }
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <p>This is a React App</p>
+        <p className={classes.join(' ')}>This is a React App</p>
         <button 
         style = {style}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
@@ -102,4 +121,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
