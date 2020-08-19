62d038b2ee4d8fb8903d67031c650af58ae01d6c
EmberLynn <ember.robb@gmail.com>
2020-08-19 10:47:29 -0600

    Created new project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 //import styled from 'styled-components';
 //import Radium, { StyleRoot } from 'radium';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import Person from '../components/Persons/Person/Person';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 
 // const StyledButton = styled.button`
 //   background-color: ${props => props.alt ? 'red' : 'green'};
@@ -102,13 +102,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}>
-              <Person
+            return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
+              key={person.id}
               changed={(event) => this.nameChangedHandler(event, person.id)} />
-              </ErrorBoundary>
           })}
         </div>
       );
